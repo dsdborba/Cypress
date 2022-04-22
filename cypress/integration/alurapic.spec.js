b7a8d6ba4605458358d4fdde7957081911ced9b9
diogo.borba <diogo.borba@dbserver.com.br>
2022-04-22 14:45:23 -0300

    atualizacao 220422

---
+++ cypress/integration/alurapic.spec.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+describe('Testes alurapic', () => {
+    beforeEach(() => {
+        cy.visit('alura-fotos.herokuapp.com');
+    });
+it('Teste de Login', () => {
+    cy.get(':nth-child(1) > .form-control').type('flavio');
+    cy.get(':nth-child(2) > .form-control').type('123');
+    cy.get('.btn').click();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
