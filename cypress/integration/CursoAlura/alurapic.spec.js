332fab3a0d17f3798373301ed9501bdbd731c83b
diogo.borba <diogo.borba@dbserver.com.br>
2022-05-04 10:14:32 -0300

    alura dia 040522

--- cypress/integration/CursoAlura/alurapic.spec.js
+++ cypress/integration/CursoAlura/alurapic.spec.js
@@ -1,15 +1,19 @@
 /// <reference types="cypress" />
 
+import '../../support/gui_commands'
+
+
+
 describe('Testes alurapic', () => {
     beforeEach(() => {
         cy.visit('alura-fotos.herokuapp.com');
     });
 it('Teste de Login', () => {
-    cy.get(':nth-child(1) > .form-control').type('flavio');
-    cy.get(':nth-child(2) > .form-control').type('123');
-    cy.get('.btn').click();
-    cy.get('a.mr-1')
-      .should('contain', 'flavio');
+  
+     cy.login('flavio', '123')
+     cy.contains('flavio').should('be.visible');
+     
+
 });
 
 
---
+++ cypress/integration/CursoAlura/api-alurapic.spec.js
@@ -0,0 +1,21 @@
+describe('Buscar fotos e dados', () => {
+
+    it('Buscar fotos do Flavio', () => {
+
+        cy.request({
+            method: 'GET',
+            url: 'https://apialurapic.herokuapp.com/flavio/photos'
+            
+        }).then((res) =>{
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body[0]).to.have.property('description')
+            expect(res.body[0].description).to.be.equal('Farol iluminado')
+        })
+
+
+        
+    });
+    
+    
+});
